7b6f8bab08ef98b8feffaf782f969f5168a8e302
Moa <moa.21@hotmail.com>
2023-10-26 17:52:54 +0200

    javascript för dropdown

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+//funktion för dropdown meny
+//tagit hjälp från w3schools
+
+
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//stäng dropdown menyn om användaren klickar utaför
+
+window.onclick = function(event) {
+    if(!event.target.matches('.dropbutton')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        //loopa igenom 
+        for(i=0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
